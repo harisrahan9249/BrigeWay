dfba6fb71a25a3bc4633a2f08fa360e30cb11125
harisrahan9249 <harisrahman9249@gmail.com>
2022-10-09 23:14:47 +0200

    logo making

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -6,7 +6,23 @@ function Navbar() {
   return (
     <div className='navbar'>
         <div className='navbar_logo'>
-            <img src="./Images/logo.jpeg" alt="logo" />
+            <div className='navbar_logo_1'></div>
+            <div className='navbar_logo_2'></div>
+            <div className='navbar_logo_3'></div>
+            <div className='navbar_logo_4'></div>
+            <div className='navbar_logo_5'></div>
+            <div className='navbar_logo_6'></div>
+            <div className='navbar_logo_7'></div>
+            <div className='navbar_logo_8'></div>
+            <div className='navbar_logo_9'></div>
+            <div className='navbar_logo_10'></div>
+            <div className='navbar_logo_11'></div>
+            <div className='navbar_logo_12'></div>
+            <div className='navbar_logo_13'></div>
+            <div className='navbar_logo_14'></div>
+            <div className='navbar_logo_15'></div>
+            <div className='navbar_logo_16'></div>
+            <div></div>
         </div>
         <div className='navbar_tag'>
             <ul>
